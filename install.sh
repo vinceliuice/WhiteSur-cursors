fe3e959aa01957d50145a4cf02c022c066e9b5c8
vinceliuice <vinceliuice@hotmail.com>
2020-08-12 16:00:07 +0800

    Initial commit

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+ROOT_UID=0
+DEST_DIR=
+
+# Destination directory
+if [ "$UID" -eq "$ROOT_UID" ]; then
+  DEST_DIR="/usr/share/icons"
+else
+  DEST_DIR="$HOME/.local/share/icons"
+fi
+
+if [ -d "$DEST_DIR/WhiteSur-cursors" ]; then
+  rm -r "$DEST_DIR/WhiteSur-cursors"
+fi
+
+cp -pr dist $DEST_DIR/WhiteSur-cursors
+
+echo "Finished..."
+
