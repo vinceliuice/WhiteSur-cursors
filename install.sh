043af2c8f3c8f7ffb8177e995435c62c09a0ec77
Fernando <33039864+tuxslack@users.noreply.github.com>
2025-01-12 15:49:47 -0300

    Check icons folder - install.sh
    Fixing this bug:
    
    cp: could not create directory '****/.local/share/icons/WhiteSur-cursors': No such file or directory

--- install.sh
+++ install.sh
@@ -4,10 +4,19 @@ ROOT_UID=0
 DEST_DIR=
 
 # Destination directory
+
 if [ "$UID" -eq "$ROOT_UID" ]; then
+
   DEST_DIR="/usr/share/icons"
+  
+  mkdir -p $DEST_DIR/WhiteSur-cursors
+  
 else
+
   DEST_DIR="$HOME/.local/share/icons"
+  
+  mkdir -p $DEST_DIR/WhiteSur-cursors
+  
 fi
 
 if [ -d "$DEST_DIR/WhiteSur-cursors" ]; then
